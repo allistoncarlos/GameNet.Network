588652c6485efd0f647a7171777e33d1581dae19
Alliston <alliston@outlook.com>
2022-05-18 15:55:23 -0300

    chore: add Pulse (1.1.0) package

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
+        .package(url: "https://github.com/kean/Pulse.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +26,8 @@ let package = Package(
         .target(
             name: "GameNet.Network",
             dependencies: [
-                .product(name: "Alamofire", package: "Alamofire")
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "Pulse", package: "Pulse")
             ]),
         .testTarget(
             name: "GameNet.NetworkTests",
