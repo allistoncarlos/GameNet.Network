8254cd988bd7994cbf811318c372a52dd6f979d6
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-01-06 16:56:59 -0300

    chore: NetworkReachability to work only on iOS/iPadOS

--- Sources/GameNet.Network/DataProviders/API/NetworkReachability.swift
+++ Sources/GameNet.Network/DataProviders/API/NetworkReachability.swift
@@ -4,32 +4,41 @@
 //  Created by Alliston Aleixo on 18/05/22.
 //
 
-import Foundation
 import Alamofire
+import Foundation
 
-public protocol NetworkReachabilityDelegate: AnyObject {
-    func showOfflineAlert()
-    func dismissOfflineAlert()
-}
+#if os(iOS)
+    public protocol NetworkReachabilityDelegate: AnyObject {
+        func showOfflineAlert()
+        func dismissOfflineAlert()
+    }
 
-public class NetworkReachability {
-    public static let shared = NetworkReachability()
-    public var delegate: NetworkReachabilityDelegate?
+    public class NetworkReachability {
 
-    let reachabilityManager = NetworkReachabilityManager(host: "www.google.com")
+        // MARK: Public
 
-    public func startNetworkMonitoring() {
-        reachabilityManager?.startListening { status in
-            switch status {
-            case .notReachable:
-                self.delegate?.showOfflineAlert()
-            case .reachable(.cellular):
-                self.delegate?.dismissOfflineAlert()
-            case .reachable(.ethernetOrWiFi):
-                self.delegate?.dismissOfflineAlert()
-            case .unknown:
-                print("Unknown network state")
+        public static let shared = NetworkReachability()
+
+        public var delegate: NetworkReachabilityDelegate?
+
+        public func startNetworkMonitoring() {
+            reachabilityManager?.startListening { status in
+                switch status {
+                case .notReachable:
+                    self.delegate?.showOfflineAlert()
+                case .reachable(.cellular):
+                    self.delegate?.dismissOfflineAlert()
+                case .reachable(.ethernetOrWiFi):
+                    self.delegate?.dismissOfflineAlert()
+                case .unknown:
+                    print("Unknown network state")
+                }
             }
         }
+
+        // MARK: Internal
+
+        let reachabilityManager = NetworkReachabilityManager(host: "www.google.com")
+
     }
-}
+#endif
