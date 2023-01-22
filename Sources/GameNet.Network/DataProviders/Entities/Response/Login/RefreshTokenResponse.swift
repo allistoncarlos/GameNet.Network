16d0dec3ad32274abf07bc7972ee0cd1e3a528d1
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-01-22 15:15:17 -0300

    chore: add refresh token entities

--- Sources/GameNet.Network/DataProviders/Entities/Response/Login/RefreshTokenResponse.swift
+++ Sources/GameNet.Network/DataProviders/Entities/Response/Login/RefreshTokenResponse.swift
@@ -1,18 +1,16 @@
 //
-//  LoginResponse.swift
+//  RefreshTokenResponse.swift
 //
-//  Created by Alliston Aleixo on 19/05/22.
+//
+//  Created by Alliston Aleixo on 22/01/23.
 //
 
 import Foundation
 
-public struct LoginResponse: Codable {
+public struct RefreshTokenResponse: Codable {
 
     // MARK: Public
 
-    public var id: String
-    public var firstName: String
-    public var lastName: String
     public var accessToken: String
     public var refreshToken: String
     public var expiresIn: Date
@@ -20,9 +18,6 @@ public struct LoginResponse: Codable {
     // MARK: Internal
 
     enum CodingKeys: String, CodingKey {
-        case id
-        case firstName
-        case lastName
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
         case expiresIn = "expires_in"
