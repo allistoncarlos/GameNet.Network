57da36f9685f6482463ba32139994653e9cdb1fa
Alliston <alliston@outlook.com>
2025-02-19 07:46:23 -0300

    [SwiftData] Feature: add Syncable protocol to Platform entity

--- Sources/GameNet.Network/Domain/Entities/Platform/Platform.swift
+++ Sources/GameNet.Network/Domain/Entities/Platform/Platform.swift
@@ -8,17 +8,20 @@ import Foundation
 import SwiftData
 
 @Model
-public class Platform: Identifiable, Equatable, Hashable {
+public class Platform: Identifiable, Equatable, Hashable, Syncable {
     // MARK: Lifecycle
-    public init(id: String?, name: String) {
+    public init(id: String?, name: String, synced: Bool = false) {
         self.id = id
         self.name = name
+        
+        self.synced = synced
     }
 
     // MARK: Public
     public var id: String?
     public var name: String
-
+    public var synced: Bool
+    
     public func toRequest() -> PlatformRequest {
         return PlatformRequest(id: id, name: name)
     }
