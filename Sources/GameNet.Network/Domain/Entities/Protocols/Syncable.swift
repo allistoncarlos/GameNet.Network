fb21ea21377710f05cd5f9ce7f2a8a72ca18313d
Alliston <alliston@outlook.com>
2025-02-19 07:46:02 -0300

    [SwiftData] Chore: add Syncable protocol

---
+++ Sources/GameNet.Network/Domain/Entities/Protocols/Syncable.swift
@@ -0,0 +1,12 @@
+//
+//  Syncable.swift
+//  GameNet.Network
+//
+//  Created by Alliston Aleixo on 19/02/25.
+//
+
+import Foundation
+
+public protocol Syncable {
+    var synced: Bool { get set }
+}
